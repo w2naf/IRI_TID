8976984e2aeaa04bda8cabffceee884ad7234ddd
Nathaniel A. Frissell <nathaniel.frissell@scranton.edu>
2024-11-16 13:04:44 -0500

    download script

---
+++ scripts/download_madrigal.sh
@@ -0,0 +1,10 @@
+globalDownload.py --verbose                         \
+    --url=http://cedar.openmadrigal.org             \
+    --outputDir=data                                \
+    --user_fullname="Nathaniel+Frissell"            \
+    --user_email=nathaniel.frissell@scranton.edu    \
+    --user_affiliation="The+University+of+Scranton" \
+    --format="hdf5"                                 \
+    --startDate="12/18/2018"                        \
+    --endDate="12/18/2018"                          \
+    --inst=8308
